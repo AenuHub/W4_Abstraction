1326e37a88a7785a97547b50652508dd3e7982f8
AenuHub <jesuskane92@gmail.com>
2024-10-22 16:11:21 +0300

    Add project files.

---
+++ W4_Abstraction/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace W4_Abstraction
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+        // one abstract method to rule them all :))
+        public abstract void PrintPosition();
+    }
+
+    public class ProjectManager : Employee
+    {
+        public override void PrintPosition()
+        {
+            Console.WriteLine($"I am {FirstName} {LastName}, and work as Project Manager in {Department} department.");
+        }
+    }
+
+    public class Designer : Employee
+    {
+        public override void PrintPosition()
+        {
+            Console.WriteLine($"I am {FirstName} {LastName}, and work as Designer in {Department} department.");
+        }
+    }
+
+
+}
---
+++ W4_Abstraction/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using W4_Abstraction;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProjectManager manager = new ProjectManager();
+        manager.FirstName = "John";
+        manager.LastName = "Wick";
+        manager.Department = "Chaos & Destruction";
+        manager.PrintPosition();
+
+        Designer dev = new Designer();
+        dev.FirstName = "Steve";
+        dev.LastName = "Jobs";
+        dev.Department = "UI/UX";
+        dev.PrintPosition();
+
+        Console.ReadKey();
+    }
+}
